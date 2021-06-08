3de22f09b028f6360a942ce2444bacca5a7cc0aa
Triston <tristonherbst97@gmail.com>
2021-06-08 14:06:22 -0500

    first commit react native tutorial

--- App.js
+++ App.js
@@ -1,12 +1,46 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { View, Text, StatusBar, StyleSheet } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View
+    nativeID='container'
+      style={{
+        backgroundColor: "#fff",
+        flex: 1,
+        flexDirection: "row", // changes main axis
+        justifyContent: "center", //main axis
+        alignItems: "center", // secondary axis
+        alignContent: 'center',
+        flexWrap: 'wrap'
+      }}
+    >
       <StatusBar style="auto" />
+      <View style={{
+        backgroundColor: 'dodgerblue',
+        width: 100,
+        height: 100
+      }}/>
+      <View style={{
+        backgroundColor: 'gold',
+        width: 100,
+        height: 100
+      }}/>
+      <View style={{
+        backgroundColor: 'tomato',
+        width: 100,
+        height: 100
+      }}/>
+      <View style={{
+        backgroundColor: 'green',
+        width: 100,
+        height: 100
+      }}/>
+      <View style={{
+        backgroundColor: 'grey',
+        width: 100,
+        height: 100
+      }}/>
     </View>
   );
 }
