bb1086461735108bdca613346c1d03ebeb647958
Triston <tristonherbst97@gmail.com>
2021-06-09 21:53:46 -0500

    text, creating text to use accross app

--- App.js
+++ App.js
@@ -1,12 +1,28 @@
 import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import AppText from "./app/config/AppText";
 import ViewImageScreen from "./app/screens/ViewImageScreen";
 import WelcomeScreen from "./app/screens/WelcomeScreen";
 
 export default function App() {
   return (
-  // <WelcomeScreen />
-  <ViewImageScreen/>
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <AppText>
+        I love Kent, this is my first react native app lets make this a lot
+        longer here is more
+      </AppText>
+      {/* <ViewImageScreen/> */}
+      {/* // <WelcomeScreen /> */}
+    </View>
   );
 }
 
-
+const styles = StyleSheet.create({
+  
+});
